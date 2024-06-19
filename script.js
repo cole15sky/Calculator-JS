28dc192903754d2c9c1de79cbb215c0a904f89e8
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-06-19 13:56:44 +0545

    Calc Interactive

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let container = document.getElementById(`container`);
+let input = document.getElementById(`inputBox`);
+let buttons = document.querySelectorAll(`button`);
 
-// for (i=0;i<10;i++){
-//     const button=document.createElement(`button`);
-//     button.id=`button${i}`;
-//     button.className=`btnClass${i}`;
-//     button.innerText = ` ${i}`;
-//     container.appendChild(button);
-// }
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(buttons => {
+    buttons.addEventListener(`click`, (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML =='AC'){
+            string ="";
+            input.value =string;
+        }
+        else if(e.target.innerHTML =='DEL'){
+            string = string.substring(0,string.length-1);
+            input.value =string;
+        }
+        else{
+        string += e.target.innerHTML;
+        input.value = string;}
 
+    })
+})
