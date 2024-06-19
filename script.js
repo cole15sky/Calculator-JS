d28bc395fa68102cd10280340d6a9bc7dec9881b
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-06-19 11:53:31 +0545

    Calc structure

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.getElementById(`container`);
+
+// for (i=0;i<10;i++){
+//     const button=document.createElement(`button`);
+//     button.id=`button${i}`;
+//     button.className=`btnClass${i}`;
+//     button.innerText = ` ${i}`;
+//     container.appendChild(button);
+// }
+
